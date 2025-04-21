fd32b48f4d0bc22bb672a500b5c125c6fa3c476e
tomas <tomascostapp@gmail.com>
2025-04-21 20:16:27 -0300

    Mobile

--- wp-content/themes/oneonetwenty/front-page.php
+++ wp-content/themes/oneonetwenty/front-page.php
@@ -142,7 +142,7 @@ get_header(); ?>
 	<div class="tcp-container">
 		<h3 class="al-title mb-12"><span><?php echo $contacto_rapido_fields['titulo'] ?></span></h3>
 		<form action="/" class="bg-white shadow-[0px_0px_35px_#11111130] p-1 flex max-w-[617px] mx-auto rounded-lg">
-			<input class="outline-none p-4 w-full" placeholder="Introduce tu dirección de correo electrónico"
+			<input class="outline-none p-4 w-full hover:scale-[103%] transition-all" placeholder="Introduce tu dirección de correo electrónico"
 				type="text">
 			<button class="text-white font-semibold py-2 px-8 rounded-lg bg-[#4CAF4F]">Contáctanos</button>
 		</form>
